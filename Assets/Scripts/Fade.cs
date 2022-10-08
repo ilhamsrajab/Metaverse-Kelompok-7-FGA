3307a307448293d00bd5a721a2a6057ba21c8240
Ilham Rajab <iamidat@outlook.com>
2022-10-09 00:23:52 +0700

    init

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.Getmo)
+    }
+}
