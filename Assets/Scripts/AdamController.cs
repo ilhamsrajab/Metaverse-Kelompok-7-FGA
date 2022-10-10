08d0be6b22f49fd848db996e267f5d9ec8f6297d
Ilham Rajab <iamidat@outlook.com>
2022-10-10 12:40:36 +0700

    gameplay

---
+++ Assets/Scripts/AdamController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdamController : MonoBehaviour
+{
+  public Vector3 target;
+  public float speed = 1.0f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
